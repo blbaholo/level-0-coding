f3d620559514975861a908c8311ae6e1706c4e33
Basetsana <basetsanabaholo@gmail.com>
2022-04-11 19:19:02 +0200

    updated Task 0.5 code

--- Task0_5.py
+++ Task0_5.py
@@ -1,12 +1,9 @@
 from math import sqrt
-
-#a,b,c represent triangle sides
-a=int(input())
-b=int(input())
-c=int(input())
-
+a=3
+b=4
+c=5
 def area_of_a_triangle(a,b,c):
-
+ 
     s=(a+b+c)/2                #s=semiperimeter
     area=sqrt(s*(s-a)*(s-b)*(s-c))
     return area
