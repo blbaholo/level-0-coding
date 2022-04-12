a81484ddad5f21cb6cda80157bcd310c961d45d8
Basetsana <basetsanabaholo@gmail.com>
2022-04-12 17:23:34 +0200

    added Task 0.8 code

---
+++ Task0_8.py
@@ -0,0 +1,17 @@
+number=121
+
+def number_to_time():
+    hours=number//60
+    minutes=number%60
+    if 2<=number<=59:
+        print(str(number) + " minutes ")
+    elif (minutes==1 and hours==1):
+        print(str(hours) + " hour, " + str(minutes) + " minute ") 
+    elif hours==1:
+        print(str(hours) + " hour," + str(minutes) + " minutes ")
+    elif minutes==1:
+        print(str(hours) + " hours, " + str(minutes) + " minute ")
+    else:
+        print(str(hours) + " hours, " + str(minutes) + " minutes ")
+
+number_to_time()
