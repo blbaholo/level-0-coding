1013afc463f1d36fc30a5ec05f0fcaf54e7dd94b
Basetsana <basetsanabaholo@gmail.com>
2022-04-16 00:26:54 +0200

    updated task 0.7 code

--- Task0_7.py
+++ Task0_7.py
@@ -1,13 +1,10 @@
-C=24                         #C=degrees Celsius
 
-def celsius_to_fahrenheit():
-    F=int((C*1.8)+32)        #F=degrees Fahrenheit
+def celsius_to_fahrenheit(C):
+    F=int((C*1.8)+32)        #C=degrees celsius,F=degrees Fahrenheit
     return F
-print(celsius_to_fahrenheit())
+print(celsius_to_fahrenheit(24))
 
-F=50
-
-def fahrenheit_to_celsius():
+def fahrenheit_to_celsius(F):
     C=int((F-32)*(5/9))
     return C
-print(fahrenheit_to_celsius())
+print(fahrenheit_to_celsius(50))
