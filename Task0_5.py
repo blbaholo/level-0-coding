1042eebc8c50b9c9dc7437f8f928986d54f91d33
Basetsana <basetsanabaholo@gmail.com>
2022-04-15 23:21:12 +0200

    fixed error in Task 0.5 code

--- Task0_5.py
+++ Task0_5.py
@@ -1,11 +1,9 @@
 from math import sqrt
-a=3
-b=4
-c=5
+
 def area_of_a_triangle(a,b,c):
- 
+
     s=(a+b+c)/2                #s=semiperimeter
     area=sqrt(s*(s-a)*(s-b)*(s-c))
     return area
 
-print(area_of_a_triangle(a,b,c))
+print(area_of_a_triangle(3,4,5))
