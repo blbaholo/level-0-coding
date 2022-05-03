da479371311c2a64502a422849fffaf95c196341
Basetsana <basetsanabaholo@gmail.com>
2022-05-03 21:16:42 +0200

    updated code for tasks 0.6,0.9 and 1.0

--- task0_9.py
+++ task0_9.py
@@ -1,26 +1,26 @@
 def vowels(word):
     word.split(word)
-    List_of_letters=[]
+    list_of_letters=[]
     for i in word:
         if (i=="u" or i=="U"):
-            List_of_letters.append("u")
+            list_of_letters.append("u")
             break
     for i in word:
         if (i=="o" or i=="O"):
-            List_of_letters.append("o")
+            list_of_letters.append("o")
             break
     for i in word:
         if (i=="i" or i=="I"):
-            List_of_letters.append("i")
+            list_of_letters.append("i")
             break
     for i in word:
         if (i=="e" or i=="E"):
-            List_of_letters.append("e")
+            list_of_letters.append("e")
             break
     for i in word:
         if (i=="a" or i=="A"):
-            List_of_letters.append("a")
+            list_of_letters.append("a")
             break
-    print("Vowels: " + ', '.join(List_of_letters))
+    print("Vowels: " + ', '.join(list_of_letters))
 
 vowels("Umuzi")
--- task1_0.py
+++ task1_0.py
@@ -1,11 +1,13 @@
 def common_letters(word1,word2):
-    word1.split(word1)
-    word2.split(word2)
-    List_of_letters=[]
+    list_of_letters=[]
+    final_list=[]
     for i in word2:
-        if (i in word1):
-            List_of_letters.append(i)
-    print("Common letters: " + ', '.join(List_of_letters))
+        if i in word1:
+            list_of_letters.append(i)
+    for i in list_of_letters:
+        if i not in final_list:
+            final_list.append(i)
+    print("Common letters: " + ', '.join(final_list))
 
-common_letters("houses","computers")
+common_letters("houses","computerss")
 
